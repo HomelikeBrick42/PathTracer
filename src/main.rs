c458757c36d4e94896892b6055f21396e0c8aa82
HomelikeBrick42 <64717463+HomelikeBrick42@users.noreply.github.com>
2021-07-18 17:14:04 +1200

    INCOMPLETE TODO: File writing

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,8 @@ use crate::ray::{ Ray, Intersectable };
 use crate::sphere::{ Sphere };
 use crate::color::{ Color };
 
+use std::fs::File;
+
 fn main() {
     let sphere = Sphere::new(Vector3::new(0.0, 0.0, 0.0), 1.0);
     let ray = Ray::new(Vector3::new(2.0, 0.0, 0.0), Vector3::new(-1.0, 0.0, 0.0).normalized());
@@ -21,4 +23,14 @@ fn main() {
         },
         _ => panic!(),
     }
+
+    let mut pixels = Vec::<Color>::new();
+    for i in 0..100 {
+        for j in 0..100 {
+            pixels.push(Color::new(i as f64, j as f64, (i * j) as f64, 1.0));
+        }
+    }
+
+    let file = File::open("./out_image.bmp").expect("Unable to create file!");
+    // TODO: Write pixels to file!
 }
